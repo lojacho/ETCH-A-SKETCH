2bd1cf121f4de459b4d4aaa17785ca28c1c5cb0e
diego <dgcuenca@gmail.com>
2022-07-20 17:08:50 -0500

    Create a grid of 16x16

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,15 @@
-var gridSize = document.getElementById("gridSize").value;
+let gridSize = document.getElementById("gridSize").value;
 
 function createGrid(gridSize) {
     const sketchPad = document.querySelector('.sketchPad');
-    sketchPad.textContent =`${gridSize}`;
-
+    const rows= gridSize;
+    const cols= rows;
+    sketchPad.style.setProperty('--grid-rows', rows);
+    sketchPad.style.setProperty('--grid-cols', cols);
+    for (let i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        sketchPad.appendChild(cell).className = "grid-item";
+      };
 }
 
 createGrid(gridSize)
