f515849f5f9ed5c259556c45ed884c5bf7b081e2
diego <dgcuenca@gmail.com>
2022-07-20 12:14:08 -0500

    Create a imput number in order to be use to create a grid with JS

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,9 @@
-function createGrid() {
-    
+var gridSize = document.getElementById("gridSize").value;
+
+function createGrid(gridSize) {
+    const sketchPad = document.querySelector('.sketchPad');
+    sketchPad.textContent =`${gridSize}`;
+
 }
+
+createGrid(gridSize)
