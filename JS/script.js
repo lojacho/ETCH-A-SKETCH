be17171fb85150d34d381c2fbaecba3115bdb2b8
diego <dgcuenca@gmail.com>
2022-07-21 17:08:19 -0500

    Enable create a grid in function de value you enter

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,35 @@
 let gridSize = document.getElementById("gridSize").value;
+document.getElementById("gridSize").addEventListener('change', createGrid);
 
 function createGrid(gridSize) {
+    gridSize = document.getElementById("gridSize").value;
     const sketchPad = document.querySelector('.sketchPad');
     const rows= gridSize;
-    const cols= rows;
+    const cols= gridSize;
     sketchPad.style.setProperty('--grid-rows', rows);
     sketchPad.style.setProperty('--grid-cols', cols);
+
+    const list = document.querySelector(".sketchPad");
+    while (list.hasChildNodes()) {
+      list.removeChild(list.firstChild);
+    }
+
     for (let i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         sketchPad.appendChild(cell).className = "grid-item";
       };
+      over();
+
 }
 
-createGrid(gridSize)
-
-const gridItems = document.querySelectorAll('.grid-item');
-[...gridItems].forEach(item => {
-  item.addEventListener('mouseover', () => {
-    item.classList.add('grid-item-over');
+function over(){
+  const gridItems = document.querySelectorAll('.grid-item');
+  [...gridItems].forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.classList.add('grid-item-over');
+    })
   })
-})
+}
+
+createGrid(gridSize);
+
